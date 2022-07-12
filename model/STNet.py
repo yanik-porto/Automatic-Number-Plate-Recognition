decb6b5d212b74ef4fb5c75e330987b70e2327b1
yporto <yporto@pryntec.com>
2022-07-12 17:22:40 +0200

    gridSample operator replacement

--- model/STNet.py
+++ model/STNet.py
@@ -27,13 +27,45 @@ class STNet(nn.Module):
         self.fc_loc[2].weight.data.zero_()
         self.fc_loc[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
         
+        self._batch_size = 1
+        self._in_ch =3
+        self._w = 94
+        self._h = 24
+        self.base_grid = self.create_base_grid(self._batch_size, self._in_ch, self._h, self._w)
+
+    def linspace_from_neg_one(self,num_steps,dtype=torch.float32,align_corners=True):
+        r = torch.linspace(-1, 1, num_steps, dtype=torch.float32)
+        r = r * (num_steps - 1) / num_steps
+        return r
+
+    def create_base_grid(self, N,C,H,W):
+        # https://github.com/kevinzakka/spatial-transformer-network/blob/master/stn/transformer.py
+        base_grid = torch.empty((N,H,W,C), dtype=torch.float32)
+        base_grid.select(-1,0).copy_(self.linspace_from_neg_one(W, dtype=torch.float32))
+        base_grid.select(-1,1).copy_(self.linspace_from_neg_one(H, dtype=torch.float32).unsqueeze_(-1))
+        base_grid.select(-1,2).fill_(1)
+        return base_grid
+
     def forward(self, x):
         xs = self.localization(x)
         xs = xs.view(-1, 32 * 14 * 2)
         theta = self.fc_loc(xs)
         theta = theta.view(-1, 2, 3)
 
-        x = self.f32fwd(x, theta)
+        N,C,H,W = x.shape
+        # if not torch.onnx.is_in_onnx_export():
+        # Move tensor to same device where input image is located
+        # Doesn't works during onnx export
+        if self.base_grid.device != x.device:
+            self.base_grid = self.base_grid.to(x.device)
+        
+        grid = self.base_grid.view(N,H*W,3).bmm(theta.transpose(1,2))
+        grid = grid.view(N, H, W, 2)
+        
+        # x = self.grid_sample(x, grid, align_corners=False)
+        x = F.grid_sample(x, grid, align_corners=True)
+
+        # x = self.f32fwd(x, theta)
 
         return x
 
